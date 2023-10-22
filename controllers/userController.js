4230e68572c8e16af756b66ded22c2c0cac4997b
lulujal <jaluadhi19@students.unnes.ac.id>
2023-10-22 17:13:23 +0700

    fixed register and login issues

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,56 +3,64 @@ const {generateToken} = require('../helpers/jwt')
 const pool =  require('../config/config')
 
 class userController {
-    static register(req, res) {
+    // user registration fixed
+    static register(req,res)  {
         const {email, password} = req.body
-        const hashedPassword = hashPassword(password)
+        const hashedPassword = hashPassword(password) // hash the password
+        const sql = `INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *`
         const search_query = `SELECT * FROM users WHERE email = '${email}'`
-        const Insert = `INSERT INTO users (email, password) VALUES ('${email}', '${hashedPassword}')`
+        
+        const values = [email, hashedPassword]
         pool.query(search_query, (error, results) => {
             if (error) {
-                throw error
+                return res.status(500).json(error)
             }
             if (results.rows.length > 0) {
-                return res.status(400).json('User already exists')
-            }
-        })
-        pool.query(Insert, (error, results) => {
-            if (error) {
-                throw error
-            }
-            res.status(201).json(results.rows)
-        })
-
-    }
-
-    static login(req, res) {
-        const {email, password} = req.body
-        const Select = `SELECT * FROM users WHERE email = '${email}'`
-        pool.query(Select, (error, results) => {
-            if (error) {
-                throw error
-            }
-            if (comparePassword(password, results.rows[0].password)) {
-                res.status(200).json(results.rows)
+                return res.status(400).json({
+                    message :"Email already used"})
             } else {
-                res.status(401).json('Email or password is invalid!')
+                pool.query(sql, values)
+                    .then(data => {
+                        res.status(201).json({
+                            id : data.rows[0].id,
+                            email : data.rows[0].email
+                        })
+                    })
+                    .catch(err => {
+                        res.status(500).json(err)
+                    })
             }
-            let payload = {
-                id: results.rows[0].id,
-                email: results.rows[0].email
-            }
-            
-            const token = generateToken(payload)
-            return res.status(200).json({
-                access_token:token
-            })
-        })
-        .catch(error => {
-            res.status(401).json(error)
-            console.log(error)
-            throw error
         })
     }
-}
+    // user login fixed
+    static login(req,res) {
+        const {email, password} = req.body
+        const search_query = `SELECT * FROM users WHERE email = '${email}'`
+        pool.query(search_query, (error, results) => {
+            if (error) {
+                return res.status(500).json(error)
+            }
+            if (results.rows.length === 0) {
+                return res.status(400).json({
+                    message :"Email not found"})
+            } else {
+                const user = results.rows[0]
+                const passwordMatch = comparePassword(password, user.password)
+                if (passwordMatch) {
+                    const access_token = generateToken({
+                        id : user.id,
+                        email : user.email
+                    })
+                    return res.status(200).json({
+                        access_token : access_token
+                    })
+                } else {
+                    return res.status(401).json({
+                        message :"Email or password invalid!"})
+                }
+            }
+        })
+    }
+}   
 
 module.exports = userController
