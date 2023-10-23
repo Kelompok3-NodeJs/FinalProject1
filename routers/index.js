efb19f3fb8e62a66fdde09aafa80eb51b7638f96
lulujal <jaluadhi19@students.unnes.ac.id>
2023-10-23 21:52:04 +0700

    menambahkan fitur authentication, create reflectio n, dan get all reflection (milik user pribadi).

--- routers/index.js
+++ routers/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 const pool = require('../config/config')
 const userController = require('../controllers/userController')
+const reflectionController = require('../controllers/reflectionController')
 const { authentication } = require('../middlewares/authentication')
+const { authorization } = require('../middlewares/authorization')
 
 router.get('/', (req, res) => {
     pool.query('SELECT * FROM reflections', (error, results) => {
@@ -16,5 +18,6 @@ router.get('/', (req, res) => {
 router.post ('/api/v1/users/register', userController.register)
 router.post('/api/v1/users/login', userController.login)
 router.use(authentication)
-
+router.get('/api/v1/reflections', reflectionController.getAll)
+router.post('/api/v1/reflections', reflectionController.create)
 module.exports = router
